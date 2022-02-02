6b31469b61079f1d463b582f067861bda8e0fd01
JackAwesomeKim <yeyjackkim23@gmail.com>
2022-02-02 10:46:18 +0900

    [ADD] image upload to AWS s3

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+
+
 //importing node framework
-var express = require('express');
- 
-var app = express();
-//Respond with "hello world" for requests that hit our root "/"
-app.get('/', function (req, res) {
- res.send('hello world');
-});
+const express = require('express')
+const cors = require('cors')
+
+const multer = require('multer')
+const upload = multer({ dest : 'uploads/' })
+
+const { uploadFile } = require('./s3')
+
+const app = express()
+app.use(cors())
+
+
+app.post('/images', upload.single('image'), async (req, res) => {
+  const file = req.file
+  console.log(file)
+  await uploadFile(file)
+  const description = req.body.description
+  res.send('completed.')
+})
+
 //listen to port 3000 by default
-// app.listen(process.env.PORT || 3000);
- 
-module.exports = app;
+app.listen(process.env.PORT || 3000)
+
+module.exports = app
--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var request = require('supertest');
-var app = require('../index.js');
+var app = require('../index_test.js');
 describe('GET /', function() {
  it('respond with hello world', function(done) {
   //navigate to root and check the response is "hello world"
