eefa9592675e49fb89c1d07e7f65da7abf82baa9
JackAwesomeKim <yeyjackkim23@gmail.com>
2022-05-03 16:09:16 +0900

    scalar added into graphql resolver and schema and take a part of the apollo server schema

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -2,9 +2,9 @@ const { mergeResolvers } = require("@graphql-tools/merge");
 const chat = require('./chat');
 const user = require('./user');
 const castingNotice = require('./castingNotice');
-
+const { date } = require('./scalars');
 const resolvers = [
-    chat, user, castingNotice
+    chat, user, castingNotice, {...date}
 ];
 
 module.exports = mergeResolvers(resolvers);
--- src/resolvers/scalars.js
+++ src/resolvers/scalars.js
@@ -4,21 +4,21 @@ const dateScalar = new GraphQLScalarType({
   name: 'Date',
   description: 'Date custom scalar type',
   serialize(value) {
-    return value.getTime(); // Convert outgoing Date to integer for JSON
+    return value; // Convert Date object from backend-side to front-side for JSON process.
   },
   parseValue(value) {
     return new Date(value); // Convert incoming integer to Date
   },
   parseLiteral(ast) {
     if (ast.kind === Kind.INT) {
-      return new Date(parseInt(ast.value, 10)); // Convert hard-coded AST string to integer and then to Date
+      return new Date(parseInt(ast.value, 10));
     }
-    return null; // Invalid hard-coded value (not an integer)
+    return null; // return with regard to the value of not an integer
   },
 });
 
-const resolvers = {
-    Date: dateScalar
-  };
 
-  module.exports = { resolvers };
+    
+const date = {Date: dateScalar};
+
+module.exports = { date };
